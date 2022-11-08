f995f23daa4f5052a6e4e9c57f940ef651fa8991
Kang-Seonggyu <jokekim609@gmail.com>
2022-11-08 14:21:40 +0900

    비교해서 변경.

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register',(req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다
   const user = new User(req.body)
-
   user.save((err, userInfo) => {
     if(err) return res.json({ success : false, err})
     return res.status(200).json({
@@ -33,7 +34,7 @@ app.post('/register',(req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email : req.body.email }, (err, user) => {
     if(!user) {
@@ -44,15 +45,23 @@ app.post('/login', (req, res) => {
     }
     // 요청된 이메일이 데이터 베이스에 있으면 비밀번호가 일치하는지 확인.
     user.comparePassword(req.body.password , (err, isMatch ) => {
-      if(!isMatch) 
-        return res.json({ loginSuccess : false, message : "비밀번호가 틀렸습니다."})
-      // 비밀번호 맞으면 토큰 생성
-      user.generateToken((err, user) => {
+      if(!isMatch) {
+        return ( res.json({ 
+          loginSuccess : false, 
+          message : "비밀번호가 틀렸습니다."
+        })
+      )
+    }
+    // 비밀번호 맞으면 토큰 생성
+    user.generateToken((err, user) => {
         if(err) return res.status(400).send(err);
-            //토큰 저장 - 위치? ⓐ쿠키 ⓑ로컬스토리지
-              res.cookie("x_auth", user.token)
-              .status(200)
-              .json({ loginSuccess:true, userId: user._id })
+          //토큰 저장 - 위치? ⓐ쿠키 ⓑ로컬스토리지
+          res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ 
+            loginSuccess:true,
+            userId: user._id 
+          })
       })
     })
   })
